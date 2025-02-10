da030e1d3c14438bbbb9c04397988914b31f2229
Alexandre Fernandez <alexandre.fernandez@enti.cat>
2025-02-10 09:45:24 +0100

    Attack enemy update

--- Assets/scripts/EnemyAttack.cs
+++ Assets/scripts/EnemyAttack.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     public int damage;
-    public void OnTriggerEnter2D(Collider2D collision)
+    public float damageInterval = 1f; // Tiempo en segundos entre cada daño
+    private bool isAttacking = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
-            PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+            StartCoroutine(DamageOverTime(collision.GetComponent<PlayerHealth>()));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isAttacking = false;
+        }
+    }
+
+    private IEnumerator DamageOverTime(PlayerHealth playerHealth)
+    {
+        isAttacking = true;
+        while (isAttacking && playerHealth != null)
+        {
             playerHealth.TakeDamagePlayer(damage);
+            yield return new WaitForSeconds(damageInterval);
         }
     }
 }
