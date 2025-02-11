efb549a8d4677f9144047293174594c744bf86a1
marccorteslon <marc.cortes@enti.cat>
2025-02-11 21:49:38 +0100

    Pause Menu
    Menú de pausa con dos botones: Continuar y Salir

---
+++ Assets/scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+
+    #if UNITY_EDITOR //Esto para el juego en Unity
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit(); //Y esto para el juego en builds
+#endif
+    }
+}
