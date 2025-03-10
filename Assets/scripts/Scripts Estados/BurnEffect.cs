fd55c3ccd694ad5d6e88cbe8ba004e0dd2018cb5
Marc Cortés <marc.cortes@enti.cat>
2025-03-10 14:22:06 +0100

    Merge pruebas

---
+++ Assets/scripts/Scripts Estados/BurnEffect.cs
@@ -0,0 +1,18 @@
+//using UnityEngine;
+
+//public class BurnEffect : StatusEffect
+//{
+//    public float damageMultiplier = 2f; // Aumenta el daño recibido un 20%
+
+//    protected override void OnEffectStart()
+//    {
+//        Debug.Log(" Fuego aplicado a " + enemyHealth.name);
+//        enemyHealth.damageMultiplier *= damageMultiplier;
+//    }
+
+//    protected override void OnEffectEnd()
+//    {
+//        Debug.Log(" El fuego en " + enemyHealth.name + " se ha extinguido.");
+//        enemyHealth.damageMultiplier /= damageMultiplier;
+//    }
+//}
